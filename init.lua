4419aa90a51a67abb4284cab62ec594ed42c8623
Guilherme Kricheldorf <gui.kricheldorf@gmail.com>
2026-04-14 14:51:05 -0300

    update format binding

--- init.lua
+++ init.lua
@@ -715,7 +715,7 @@ require('lazy').setup({
     cmd = { 'ConformInfo' },
     keys = {
       {
-        '<leader>f',
+        '<leader>cf',
         function() require('conform').format { async = true, lsp_format = 'fallback' } end,
         mode = '',
         desc = '[F]ormat buffer',
